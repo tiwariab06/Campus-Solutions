37843e49599e2f2213a3513a6b33466ef503faf0
tiwariab06 <tiwariab06@gmail.com>
2024-07-23 17:07:59 +0530

    update 3rd

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,5 +1,5 @@
 const JWT = require("jsonwebtoken");
-const secret = "@$36$biradri";
+const env = require("dotenv").config();
 
 function createTokenForUser(user) {
   const payload = {
@@ -7,14 +7,14 @@ function createTokenForUser(user) {
     fullName: user.fullName,
     email: user.email,
   };
-  const token = JWT.sign(payload, secret, {
+  const token = JWT.sign(payload, process.env.secret, {
     expiresIn: "1h",
   });
   return token;
 }
 
 function validateToken(token) {
-  const payload = JWT.verify(token, secret);
+  const payload = JWT.verify(token, process.env.secret);
   return payload;
 }
 module.exports = {
